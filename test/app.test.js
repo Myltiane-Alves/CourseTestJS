adab5b97ae838bce1427e9669214b4cdeeb76456
Myltiane Alves <myltiane.alves@cleandev.com.br>
2022-04-15 13:18:17 -0300

    feat: Criando testes de usuário

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+
+const app = require('../src/app');
+
+test('Devo responder na pasta raiz', () => {
+    return request(app).get('/')
+     .then((res) => {
+        expect(res.status).toBe(200);
+     })
+});
